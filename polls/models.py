3fbfb6613cb86fe22f42f08f600e7431cd62c9d0
nachovilla03 <nacho.villanueva@onesearch.com.uy>
2023-04-28 14:06:27 -0300

    Ajustamos settings.py, ORM, Creando los modelos questions y choice

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    # id ---> No hace falta crearlo pq django lo genera automaticamente 
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) 
+    #Esto ultimo significa que cada vez que se borre una pregunta, se van a borrar en cascada todas las choices de ella
+    choice_text = models.CharField(max_length=200) 
+    votes = models.IntegerField(default=0)  
+    
